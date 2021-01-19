8116ed35a3658a04d10584b740f263387dd45eb5
arkadiibasovych <arkadiybasovich@gmai.com>
2021-01-19 23:49:17 +0200

    updated

--- src/index.js
+++ src/index.js
@@ -14,19 +14,41 @@ refs.searchForm.addEventListener('submit', e => {
     form.reset();
 
     newsService.resetPage();
-   
+    refs.showMoreButton.classList.add('is-hidden');
+    refs.clearButton.classList.add('is-hidden');
+    refs.spinner.classList.remove('is-hidden');
 
     newsService.fetchArticles().then(articles => {
         updateArticlesMarkup(articles);
-        
-    } );
+        refs.showMoreButton.classList.remove('is-hidden');
+        refs.clearButton.classList.remove('is-hidden');
+    } ).finally(() => {refs.spinner.classList.add('is-hidden');
+});
 
 });
 
 refs.showMoreButton.addEventListener('click', () => {
+    refs.showMoreButton.classList.add('is-hidden');
+    refs.spinner.classList.remove('is-hidden');
     newsService.fetchArticles().then(articles => {
         updateArticlesMarkup(articles);
+
+        const documentHeight = document.documentElement.offsetHeight;
+
+        window.scrollTo({
+            top: documentHeight,
+           
+        })
         
-    } );
+    }).finally(() => {
+        refs.spinner.classList.add('is-hidden');
+        refs.showMoreButton.classList.remove('is-hidden')}
+    );
 })
+
+refs.clearButton.addEventListener('click', () => {
+    refs.articlesContainer.innerHTML = '';
+    refs.showMoreButton.classList.add('is-hidden');
+    refs.clearButton.classList.add('is-hidden');
+});
 
--- src/js/refs.js
+++ src/js/refs.js
@@ -3,6 +3,8 @@
 const refs = {
     articlesContainer: document.querySelector('.js-artciles'),
     searchForm: document.querySelector('.js-search-form'),
-    showMoreButton: document.querySelector('.button-show-more')
+    showMoreButton: document.querySelector('.button-show-more'),
+    clearButton: document.querySelector('.clear-button'),
+    spinner: document.querySelector('.spinner')
 };
 export default refs;
